d36d8d81c3395fd37791e99831f2c904d09978cd
Based-One <aesteban@cetys.edu.mx>
2025-10-30 20:37:50 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
